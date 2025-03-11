8e9a6aad4d8fa8cf0e1b0ec958b05745d6487743
Winchito <spartan89632@hotmail.com>
2025-03-10 22:41:20 -0500

    aaaaaaaaaaaaaaa

--- Source/TempSebas2/Private/TM_Character.cpp
+++ Source/TempSebas2/Private/TM_Character.cpp
@@ -2,12 +2,19 @@
 
 
 #include "TM_Character.h"
+#include "Camera\CameraComponent.h"
 
 // Sets default values
 ATM_Character::ATM_Character()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	FPSCameraSocketName = "SCK_Camera";
+
+	FPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FPS_CameraComponent"));
+	FPSCameraComponent-> bUsePawnControlRotation = true;
+	FPSCameraComponent-> SetupAttachment(GetMesh(), FPSCameraSocketName);
 
 }
 
@@ -38,12 +45,19 @@ void ATM_Character::StopJumping()
 	Super::StopJumping();
 }
 
+void ATM_Character::AddControllerPitchInput(float value)
+{
+	//bIsLookInversion ? Super::AddControllerPitchInput(-value) : Super::AddControllerPitchInput(value);
+	Super::AddControllerPitchInput(bIsLookInversion ? -value : value);
+}
+
 // Called every frame
 void ATM_Character::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
+
 
 // Called to bind functionality to input
 void ATM_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -54,7 +68,11 @@ void ATM_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 	PlayerInputComponent->BindAxis("MoveForward", this, &ATM_Character::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &ATM_Character::MoveRight);
 
+	PlayerInputComponent->BindAxis("LookUp", this, &ATM_Character::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookRight", this, &ACharacter::AddControllerYawInput);
+
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ATM_Character::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ATM_Character::StopJumping);
 }
+
 
